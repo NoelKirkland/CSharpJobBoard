995c22bbded808f4529d6864664cd1ed38ca75fe
Taylor Somers <tsomers@yandex.com>
2020-07-22 18:00:11 -0400

    Write failing test for JobOpening constructor
    Co-authored-by: Noel Kirkland <noelkirkland@gmail.com>

---
+++ JobBoard.Tests/ModelTests/JobOpening.Tests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using JobBoard.Models;
+using System;
+
+namespace JobBoard.TestTools
+{
+  [TestClass]
+  public class JobOpeningTest : IDisposable
+  {
+    public void Dispose()
+    {
+      JobOpening.ClearAll();
+    }
+    [TestMethod]
+    public void JobOpeningConstructor_CreatesInstanceOfJobOpening_JobOpening()
+    {
+      JobOpening newJobOpening = new JobOpening("Salt Miner", "mine salt", "Sterling Holmes", 1234567845);
+      Assert.AreEqual(typeof(int), newJobOpening.GetType());
+    }
+  }
+}
